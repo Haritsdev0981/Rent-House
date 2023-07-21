6febc350c07d8274784f2bb5cca57fe4c94166fe
Harits <haritsdeveloper@gmail.com>
2023-07-21 09:20:57 +0700

    Add Http

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,8 +31,10 @@ export class HomeComponent {
   filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
   filterResults(text: string) {
     if (!text) {
